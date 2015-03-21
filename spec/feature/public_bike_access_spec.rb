a04b5de4dcd8bbbee533560a9f36cfb12faaf446
sanjsanj <sanjsanj@hotmail.com>
2015-03-21 19:20:14 +0000

    Create docking_station unit test

--- spec/feature/public_bike_access_spec.rb
+++ spec/feature/public_bike_access_spec.rb
@@ -1,10 +1,10 @@
-require 'capybara/rspec' # this allows us to use the terms 'feature' and 'scenario' below -
-#we're describing our high level feature
+require 'docking_station'
+require 'capybara/rspec'
+
 feature 'member of public accesses bike' do
-  # this is a specific outcome related to the feature
   scenario 'docking station releases a bike that is not broken' do
-    docking_station = DockingStation.new # initialize a new object, an instance of the DockingStation class
-    bike = docking_station.release_bike  # ask the docking station to release a bike
-    expect(bike).not_to be_broken        # expect that bike to respond to the method 'broken?' with false
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike).not_to be_broken
   end
 end
