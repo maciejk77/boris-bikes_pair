2ce67af9f50a9193a3d19a6d38bf0a498f69fb82
Maciej Kurek <maciejk77@gmail.com>
2015-03-21 21:26:44 +0000

    Stage 5 including setting private methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
   it 'can dock a bike' do
-    expect(subject.dock :bike).to eq :bike
+    expect(subject.dock :bike).to be nil
   end
   it 'raises error when no bikes available' do
     expect { subject.release_bike }.to raise_error 'No Bikes Available'
@@ -13,7 +13,7 @@ describe DockingStation do
   	expect(bike).not_to be_broken
   end
   it 'raises an error when full' do
-    subject.dock double :bike
+    20.times { subject.dock double :bike }
     expect { subject.dock double :bike }.to raise_error 'Station Full'
   end
 end
